7da7ff748226c9eefe81aa5554ffb94c100873ba
diva <divamsouza28@gmail.com>
2026-02-13 14:14:41 -0300

    commit inicial do spring boot

---
+++ src/main/java/br/com/alura/screensound/principal/Principal.java
@@ -0,0 +1,11 @@
+package br.com.alura.screensound.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner entrada = new Scanner(System.in);
+
+    public void exibeMenu() {
+    }
+}
